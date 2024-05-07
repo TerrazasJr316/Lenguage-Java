2aaa14b8dac2fe74c416aefd8b89fbf7713c97fb
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-05-06 19:51:05 -0600

    Update  HolaMundo.java

--- Capitulo 1: Hola Mundo - Salida de dator por pantalla/1.1 Hola Mundo - Mi primer programa/ HolaMundo.java
+++ Capitulo 1: Hola Mundo - Salida de dator por pantalla/1.1 Hola Mundo - Mi primer programa/ HolaMundo.java
@@ -1,6 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
-        System.out.print("¡Hola Mundo!");
+        System.out.println("¡Hola Mundo!"); //la salida muestra lo que esta en parentesis pero con slato de linea.
+        System.out.print("¡Hola Mundo!"); //en el caso de esta salida no hace salto de linea.
+        //lo que difrencia las lineas 3 y 4 es por que en la linea 3 al final del "print" se agrega "ln" refiriendose a salto de linea
     }
 }
