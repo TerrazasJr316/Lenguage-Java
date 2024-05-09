89480e4571201ec282816d75aa81a084bd7212c4
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-05-08 18:10:18 -0600

    Delete Capitulo 1: Hola Mundo - Salida de datos por pantalla/Ejemplos/HolaMundo.java

--- Capitulo 1: Hola Mundo - Salida de datos por pantalla/Ejemplos/HolaMundo.java
+++
@@ -1,8 +0,0 @@
-package Ejemplos;
-
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
-        System.out.print("¡Hola Mundo!");
-    }
-}
