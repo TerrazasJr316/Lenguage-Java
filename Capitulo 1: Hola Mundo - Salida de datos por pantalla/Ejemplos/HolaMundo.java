046c7df279af110e8a0d05af4a4a92f50d5bf7fb
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-05-08 18:07:51 -0600

    Add files via upload

---
+++ Capitulo 1: Hola Mundo - Salida de datos por pantalla/Ejemplos/HolaMundo.java
@@ -0,0 +1,8 @@
+package Ejemplos;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+        System.out.print("¡Hola Mundo!");
+    }
+}
