d6809aec551d8592840fb6ad270c29ed418887ba
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-05-26 21:42:17 -0600

    Add files via upload

---
+++ Menu/com/mycopany/unidad5/ArreglosUni.java
@@ -0,0 +1,11 @@
+package com.mycompany.unidad5;
+
+public class ArreglosUni {
+    
+    public int promedioMateria (int v, int w, int x, int y, int z) {
+        int p;
+        
+        p = (79 + 77 + 71 + 100 + 100)/5;
+        return p;
+    }    
+}
