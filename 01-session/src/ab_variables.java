b3d3fabf89634ba528acb4d7a4353b56284ff409
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-07-30 23:38:26 -0600

    variables

---
+++ 01-session/src/ab_variables.java
@@ -0,0 +1,17 @@
+public class ab_variables {
+
+    public static void main(String[] args) {
+
+        // String (cadenas de texto)
+        String saludar = "Hola mundo desde Java";
+
+        // Númericos
+        byte numero = 10;
+        int numero2 = 5;
+
+        // boolean
+        boolean valor = true;
+
+        System.out.println(saludar);
+    }
+}
