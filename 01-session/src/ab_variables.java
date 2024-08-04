0d8b473b36a21e8396a30c8bec86535514f6604f
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-03 21:10:29 -0600

    variables

--- 01-session/src/ab_variables.java
+++ 01-session/src/ab_variables.java
@@ -23,5 +23,13 @@ public class ab_variables {
         System.out.println("tipo byte corresponde en byte a " + Byte.SIZE);
         System.out.println("valor máximo de un byte " + Byte.MAX_VALUE);
         System.out.println("valor mínimo de un byte " + Byte.MIN_VALUE);
+
+        short numeroShort = 30000;
+
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("tipo short corresponde en bite a " + Short.BYTES);
+        System.out.println("tipo short corresponde en bite a " + Short.SIZE);
+        System.out.println("valor máximo de un short " + Short.MAX_VALUE);
+        System.out.println("valor mínimo de un short " + Short.MIN_VALUE);
     }
 }
